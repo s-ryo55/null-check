cee405d12ac9d3d9956cde8f48e9383324bc5fd2
4D-15\koyo <a>
2026-06-09 13:56:28 +0900

    shared_ptrを使った設計とコピー

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,37 +5,33 @@
 #include <iostream>
 #include<assert.h>
 
-class Test
-{
-public:
-    void show() 
-    {
-        std::cout << "Helle, World!!" << std::endl;
-    }
 
+class Weapon {
+public:
+	void Use() {}
 };
 
-void Func(Test* test) 
-{
-    assert(test != nullptr);
-    test->show();
+class Player {
+	std::shared_ptr<Weapon> weapon;
+public:
+	Player(std::shared_ptr<Weapon> weapon1)
+		: weapon(std::move(weapon1)) {
+		std::cout << weapon.use_count() << std::endl;
+		std::cout << weapon1.use_count() << std::endl;
+
+	}
+
+	void Attack() {
+		weapon->Use();
+	}
+};
+
+int main() {
+	auto wp = std::make_shared<Weapon>();
+
+	Player p(wp);
+
+	Player q(std::move(wp));
+	return 0;
+
 }
-
-
-int main()
-{
-    Func(new Test());
-    Func(nullptr);
-
-}
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
